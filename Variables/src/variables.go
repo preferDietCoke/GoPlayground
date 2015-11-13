e0792baea7e4934477b41fd92a2c4f4fcec6c923
Hasan Pektas <hasan.pektas@icloud.com>
2015-11-13 19:55:15 +0100

    Still playing around with Go

---
+++ Variables/src/variables.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var initString string = "initial"
+	fmt.Println(initString)
+
+	var numberOne, numberTwo int = 1, 2
+	fmt.Println(numberOne, numberTwo)
+
+	var booleanVal bool = true
+	fmt.Println(booleanVal)
+
+	var unsignedInt int
+	fmt.Println(unsignedInt)
+
+	// should I specify type?
+	shortDeclaration := "short"
+	fmt.Println(shortDeclaration)
+}
+
+/*
+	Unsigned values gets a standard value.
+	String value gets ""
+	Int value get 0
+	and so on.
+*/
